0231f856d07cdba6da93541a829e82c19feed025
ArzumSonmez <arzum_sonmez@hotmail.com>
2024-12-16 16:05:16 +0300

    generated the order summary html and fixed errors

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,6 +1,7 @@
 //imported files
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from "./utils/money.js";
 
 
 //creating an undefined productsHTML to add the values we find after the loop.
@@ -29,7 +30,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
