4b9b0102f0e36c04015fe7d022c9074e4451311e
ArzumSonmez <arzum_sonmez@hotmail.com>
2024-12-21 17:39:46 +0300

    importing delete function for the cart item removing

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+import { removeFromCart } from "../data/cart.js";
 
 let cartSummaryHTML = '';
 
@@ -41,7 +42,7 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary js-delete-link">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -102,10 +103,7 @@ document.querySelector('.js-order-summary')
 document.querySelectorAll('.js-delete-link')
 .forEach((link) => {
   link.addEventListener('click', () => {
-    console.log('delete');
+    const  productId = link.dataset.productId;
+    removeFromCart(productId);
   });
 });
-
-  /*const links = document.querySelectorAll('.js-delete-link');
-  console.log('Links NodeList:', links);
-  console.log('Number of links:', links.length);*/
