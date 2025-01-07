7c22a3606177329a0cfec5c070d3b964b8f50c52
ArzumSonmez <arzum_sonmez@hotmail.com>
2025-01-07 22:47:58 +0300

    added interactivity to the delivery options

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -4,6 +4,7 @@ import { formatCurrency } from "./utils/money.js";
 import { removeFromCart } from "../data/cart.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions } from "../data/deliveryOptions.js";
+import { updateDeliveryOption } from "../data/cart.js";
 
 /*const today = dayjs();
 const deliveryDate = today.add(7, 'days');
@@ -101,7 +102,8 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html += 
-    `<div class="delivery-option">
+    `<div class="delivery-option js-delivery-option"  data-product-id="${matchingProduct.id}"
+    data-delivery-option-id="${deliveryOption.id}">
       <input type="radio"
         ${isChecked ? 'checked' : ''}
         class="delivery-option-input"
@@ -151,3 +153,10 @@ function updateCartQuantity(){
   };
   
   updateCartQuantity();
+
+  document.querySelectorAll('.js-delivery-option').forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    });
+  });
