c568f1ea9f4ecb874f1ba72e87eb5d9924ee1186
ArzumSonmez <arzum_sonmez@hotmail.com>
2025-01-11 17:09:10 +0300

    header update function works properly

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -112,7 +112,7 @@ document.querySelectorAll('.js-delete-link')
        `.js-cart-item-container-${productId}`
     );
     container.remove();
-
+    updateCartQuantity();
     renderPaymentSummary();
   });
 });
