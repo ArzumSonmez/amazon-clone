c92634bbf065023d9cf6c24c457b553e9c86c88f
ArzumSonmez <arzum_sonmez@hotmail.com>
2025-01-21 17:43:17 +0300

    commit all

---
+++ javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -0,0 +1,40 @@
+import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+
+//to test an if statement test all the existing conditions
+describe('test suite: addToCart', () => {
+  it('adds an existing product to the cart', () => {
+    spyOn(localStorage, 'setItem');
+
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]); //sets the cart for testing
+    });
+    loadFromStorage();
+
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(2);
+  });
+
+
+  //creating a mock version of getItem to test flaky test.
+  it('adds a new product to the cart', () =>{
+    spyOn(localStorage, 'setItem');
+
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([]); //makes getItem return an empty array.
+    });
+    loadFromStorage();
+
+    addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+  });
+});
