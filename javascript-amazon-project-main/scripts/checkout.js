5871ed68c755e9c670c5a2b58720b04603f1ebca
ArzumSonmez <arzum_sonmez@hotmail.com>
2025-01-29 21:57:06 +0300

    fetching products through backend code

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,13 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js';
-renderOrderSummary();
-renderPaymentSummary();
+//import '../data/backend-practice.js'
+
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+
+
